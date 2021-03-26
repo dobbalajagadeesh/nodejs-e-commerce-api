2f5187e47c6735baee38dfdcc24437e6d17ed792
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-26 18:39:49 +0530

    feat: configure body-parser middleware

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', (req, res)=>{
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+//middlewares
+app.use(bodyParser.json());
+
+app.get(`${api}/products`, (req, res)=>{
     res.send('API Works');
 })
 app.listen(3000, ()=>{
