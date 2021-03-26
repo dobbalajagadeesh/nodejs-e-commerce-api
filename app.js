4360498ea5d4ba097b23250df4c85b412cc2b490
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-26 21:05:37 +0530

    feat: finish api

--- app.js
+++ app.js
@@ -2,31 +2,57 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
 
 require('dotenv/config');
 
-const api = process.env.API_URL;
-const productRouter = require('./routes/products');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
-//middlewares
-app.use(bodyParser.json());
+app.use(cors());
+app.options('*',cors());
+
+// Middlewares
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static( __dirname + '/public/uploads'));
+app.use(errorHandler);
 
-app.use(`${api}/products`, productRouter)
+const api = process.env.API_URL;
+const categoriesRoute = require('./routes/categories');
+const productRoute = require('./routes/products');
+const userRoute = require('./routes/users');
+const orderRoute = require('./routes/orders');
 
-mongoose.connect(process.env.CONNECTION_STRING, {
+// Routes
+
+app.use(`${api}/products`, productRoute);
+app.use(`${api}/categories`, categoriesRoute);
+app.use(`${api}/users`, userRoute);
+app.use(`${api}/orders`, orderRoute);
+
+const dbConfig = require('./config/database.config.js');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-})
-.then(()=>{
-    console.log('Database Connected');
-})
-.catch((err)=>{
-    console.log(err);
+
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
 });
 
-app.listen(3000, ()=>{
-    console.log('Server running on port 3000');
-})
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
