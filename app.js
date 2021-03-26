7f159da2fc6f4408fd9d262e9f50498d0ffae2cb
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-26 19:12:13 +0530

    feat: established database connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 
 require('dotenv/config');
@@ -14,6 +15,19 @@ app.use(morgan('tiny'));
 app.get(`${api}/products`, (req, res)=>{
     res.send('API Works');
 })
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+.then(()=>{
+    console.log('Database Connected');
+})
+.catch((err)=>{
+    console.log(err);
+});
+
 app.listen(3000, ()=>{
     console.log('Server running on port 3000');
 })
